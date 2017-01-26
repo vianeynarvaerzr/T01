47a69c61c59ade2395fe87ea8620c0e3e1fe44e9
Vianey_Narváez <Vianey_Narvez@192.168.1.70>
2017-01-26 16:57:25 -0600

    Tarea 01. Problema 1

---
+++ src/spp2/lnarvaezr/t01/SPP2LNarvaezRT01.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spp2.lnarvaezr.t01;
+import java.util.Scanner;
+/**
+ *
+ * @author Vianey_Narváez
+ */
+public class SPP2LNarvaezRT01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
