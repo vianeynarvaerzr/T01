fdddc58f1fb8b2494ff557a9508018d344d77a22
vianeynarvaerzr <A01410432@itesm.mx>
2017-01-26 21:42:00 -0600

    T01-P1
    Leticia Vianey Narváez Reyes A01410432
    Ingenieria Industrial y de Sistemas

--- src/spp2/lnarvaezr/t01/SPP2LNarvaezRT01.java
+++ src/spp2/lnarvaezr/t01/SPP2LNarvaezRT01.java
@@ -1,4 +1,4 @@
-/*
+/* //Leticia Vianey Narvaez Reyes A01410432 Ingenieria Industrial y de Sistemas
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
